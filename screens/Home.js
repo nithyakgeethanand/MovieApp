ba4f57e128e3193ac77a50e3ff106719b8058880
nithyakgeethanand <67506979+nithyakgeethanand@users.noreply.github.com>
2024-03-14 14:40:29 +0530

    Added colors constant and rating

--- screens/Home.js
+++ screens/Home.js
@@ -4,6 +4,8 @@ import { useQuery } from 'react-query';
 import { getAllMovies } from '../queries/movies';
 import LoadingElement from '../components/LoadingElement';
 import movie_poster from '../assets/movie_poster.png';
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../utils/colors';
 
 const Home = () => {
     const { data, isLoading, isError } = useQuery('movies', getAllMovies);
@@ -29,7 +31,10 @@ const Home = () => {
                 />
                 <View style={styles.movieDesc}>
                     <Text style={styles.movieHeader}>{item.movie}</Text>
-                    <Text style={styles.rating}>{item.rating}</Text>
+                    <View style={styles.ratingContainer}>
+                        <AntDesign name="star" size={24} color={colors.ratingStar} />
+                        <Text style={styles.rating}>{item.rating}</Text>
+                    </View>
                 </View>
             </View>
         )
@@ -50,10 +55,10 @@ export default Home
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#FFFFFF',
+        backgroundColor: colors.background,
     },
     flatListConatiner: {
-        backgroundColor: '#F2F1F0',
+        backgroundColor: colors.movieCardbackground,
         flex: 1,
         flexDirection: 'row',
         borderRadius: 10,
@@ -61,24 +66,31 @@ const styles = StyleSheet.create({
         padding: 10
     },
     moviePoster: {
-        width: 100, 
-        height: 100, 
+        width: 100,
+        height: 100,
         alignContent: 'center',
         borderRadius: 10
     },
     movieHeader: {
         fontSize: 20,
-        fontWeight:'500',
-        
+        fontWeight: '500',
+
     },
     movieDesc: {
         padding: 20,
         width: 250
     },
+    ratingContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingTop: 5
+    },
     rating: {
         fontSize: 15,
-        paddingTop : 30
+        marginLeft: 5,
+        fontWeight: "500",
+        
     }
-    
+
 });
 
---
+++ utils/colors.js
@@ -0,0 +1,5 @@
+export default {
+    ratingStar: "#E5200F",
+    background: "#FFFFFF",
+    movieCardbackground: "#F2F1F0"
+}
